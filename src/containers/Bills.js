7708f54fb222252f648bd6feeb724f17c09e430f
Guillaume <101353118+cordellier@users.noreply.github.com>
2024-05-15 16:22:19 +0200

    Update Bills.js

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -25,6 +25,7 @@ export default class {
   };
 
   handleClickIconEye = (icon) => {
+    // console.log(icon);
     const billUrl = icon.getAttribute("data-bill-url");
     const imgWidth = Math.floor($("#modaleFile").width() * 0.5);
     $("#modaleFile")
@@ -53,7 +54,7 @@ export default class {
               } catch (e) {
                 // if for some reason, corrupted data was introduced, we manage here failing formatDate function
                 // log the error and return unformatted date in that case
-                console.log(e, "for", doc);
+                // console.log(e,'for',doc)
                 return {
                   ...doc,
                   date: doc.date,
@@ -61,7 +62,7 @@ export default class {
                 };
               }
             });
-          console.log("length", bills.length);
+          // console.log('length', bills.length)
           return bills;
         });
     }
