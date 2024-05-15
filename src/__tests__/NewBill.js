441e8e6056df84922260b58486c5e1fdbb3ca81e
Guillaume <101353118+cordellier@users.noreply.github.com>
2024-05-15 16:21:01 +0200

    Update NewBill.js

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,18 +2,20 @@
  * @jest-environment jsdom
  */
 
-
 import NewBill from "../containers/NewBill.js";
 
-
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    describe("when uploading a file with the correct format", () => {
-      test("should save the user's email", () => {
-        // Mocking functions and data
+describe("Étant donné que je suis connecté en tant qu'employé", () => {
+  describe("Lorsque je suis sur la page de Nouvelle note de frais", () => {
+    describe("lorsque je télécharge un fichier avec le format correct", () => {
+      test("devrait enregistrer l'e-mail de l'utilisateur", () => {
+        // Fonctions et données fictives
         const mockGetElementById = jest.fn().mockReturnValue({});
-        const createMock = jest.fn().mockResolvedValue({ fileUrl: "fileURL", key: "key" });
-        const goodFormatFile = new File(['img'], 'image.png', { type: 'image/png' });
+        const createMock = jest
+          .fn()
+          .mockResolvedValue({ fileUrl: "fileURL", key: "key" });
+        const goodFormatFile = new File(["img"], "image.png", {
+          type: "image/png",
+        });
 
         const documentMock = {
           querySelector: (selector) => {
@@ -29,10 +31,10 @@ describe("Given I am connected as an employee", () => {
           getElementById: mockGetElementById,
         };
 
-        // Set up localStorage
+        // Configuration de localStorage
         localStorage.setItem("user", '{"email" : "user@email.com"}');
 
-        // Setting up test instance
+        // Configuration de l'instance de test
         const storeMock = {
           bills: () => ({
             create: createMock,
@@ -45,27 +47,29 @@ describe("Given I am connected as an employee", () => {
           localStorage: {},
         });
 
-        // Triggering file upload
+        // Déclenchement du téléchargement de fichier
         objInstance.handleChangeFile({
           preventDefault: jest.fn(),
           target: { value: "image.png" },
         });
 
-        // Expectations
+        // Assertions
         const expectedEmail = "user@email.com";
         const formData = createMock.mock.calls[0][0].data;
-        console.log('formData', formData);
+        console.log("formData", formData);
 
         expect(formData.get("email")).toEqual(expectedEmail);
       });
     });
 
-    describe('when submitting a new bill', () => {
-      test('should call the update method on the store', () => {
-        // Mocking functions and data
+    describe("lorsque je soumets une nouvelle note de frais", () => {
+      test("devrait appeler la méthode update sur le store", () => {
+        // Fonctions et données fictives
         const mockGetElementById = jest.fn().mockReturnValue({});
         const createMock = jest.fn();
-        const goodFormatFile = new File(['img'], 'image.png', { type: 'image/png' });
+        const goodFormatFile = new File(["img"], "image.png", {
+          type: "image/png",
+        });
         const mockUpdate = jest.fn().mockResolvedValue({});
         const documentMock = {
           querySelector: (selector) => {
@@ -86,7 +90,7 @@ describe("Given I am connected as an employee", () => {
           }),
         };
 
-        // Setting up test instance
+        // Configuration de l'instance de test
         const objInstance = new NewBill({
           document: documentMock,
           onNavigate: jest.fn(),
@@ -94,52 +98,52 @@ describe("Given I am connected as an employee", () => {
           localStorage: {},
         });
 
-        // Triggering form submission
+        // Déclenchement de la soumission du formulaire
         objInstance.handleSubmit({
           preventDefault: jest.fn(),
           target: {
             querySelector: (selector) => {
               switch (selector) {
                 case 'select[data-testid="expense-type"]':
-                  return { value: 'type' };
+                  return { value: "type" };
                 case 'input[data-testid="expense-name"]':
-                  return { value: 'name' };
+                  return { value: "name" };
                 case 'input[data-testid="amount"]':
-                  return { value: '3000' };
+                  return { value: "3000" };
                 case 'input[data-testid="datepicker"]':
-                  return { value: 'date' };
+                  return { value: "date" };
                 case 'input[data-testid="vat"]':
-                  return { value: 'vat' };
+                  return { value: "vat" };
                 case 'input[data-testid="pct"]':
-                  return { value: '25' };
+                  return { value: "25" };
                 case 'textarea[data-testid="commentary"]':
-                  return { value: 'commentary' };
+                  return { value: "commentary" };
               }
             },
           },
         });
 
-        // Expectations
+        // Assertions
         const dataToCheck = {
-          email: 'user@email.com',
-          type: 'type',
-          name: 'name',
+          email: "user@email.com",
+          type: "type",
+          name: "name",
           amount: 3000,
-          date: 'date',
-          vat: 'vat',
+          date: "date",
+          vat: "vat",
           pct: 25,
-          commentary: 'commentary',
+          commentary: "commentary",
           fileUrl: null,
           fileName: null,
-          status: 'pending',
+          status: "pending",
         };
 
-        // Analyzing the data passed to the function
+        // Analyse des données transmises à la fonction
         const data = JSON.parse(mockUpdate.mock.calls[0][0].data);
-        console.log('data?', data);
+        console.log("data?", data);
 
         expect(data).toMatchObject(dataToCheck);
       });
     });
   });
-});  
+});
